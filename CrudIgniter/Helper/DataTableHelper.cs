3ac576de8323cc4ca59757ce546c6a0240f98a51
azjhe <arjayazana25@gmail.com>
2019-03-22 11:16:35 +0800

    Added DataTableHelper

---
+++ CrudIgniter/Helper/DataTableHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+
+namespace CrudIgniter.Helper 
+{
+
+    // source: https://www.codeproject.com/Tips/1099251/Convert-DataTable-to-List-of-Object
+
+    public static class DataTableHelper
+    {
+        public static T ToObject<T>(this DataRow row) where T : class, new()
+        {
+            T obj = new T();
+
+            foreach (var prop in obj.GetType().GetProperties())
+            {
+                try
+                {
+                    if (prop.PropertyType.IsGenericType && prop.PropertyType.Name.Contains("Nullable"))
+                    {
+                        if (!string.IsNullOrEmpty(row[prop.Name].ToString()))
+                            prop.SetValue(obj, Convert.ChangeType(row[prop.Name],
+                            Nullable.GetUnderlyingType(prop.PropertyType), null));
+                        //else do nothing
+                    }
+                    else
+                        prop.SetValue(obj, Convert.ChangeType(row[prop.Name], prop.PropertyType), null);
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            return obj;
+        }
+
+        /// <summary>
+        /// Converts a DataTable to a list with generic objects
+        /// </summary>
+        /// <typeparam name="T">Generic object</typeparam>
+        /// <param name="table">DataTable</param>
+        /// <returns>List with generic objects</returns>
+        public static List<T> ToList<T>(this DataTable table) where T : class, new()
+        {
+            try
+            {
+                List<T> list = new List<T>();
+
+                foreach (var row in table.AsEnumerable())
+                {
+                    var obj = row.ToObject<T>();
+
+                    list.Add(obj);
+                }
+
+                return list;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
